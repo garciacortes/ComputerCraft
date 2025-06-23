8317a82e455d7b6c3b008a4153c9c494924ef6e7
garciacortes <55157556+garciacortes@users.noreply.github.com>
2025-06-23 15:49:43 -0300

    Create arquivos_ids.lua

---
+++ Chat_De_Texto/arquivos_ids.lua
@@ -0,0 +1,19 @@
+return {
+    ["chat_client.lua"] = "6yTXZnju",
+    ["chat_server.lua"] = "b15WbJdn",
+    ["menu.lua"] = "EJQ2PzKr",
+    ["versao.lua"] = "g3ykXWf5",
+    ["atualizar_server.lua"] = "1i2nFHi6",
+    ["calculadora.lua"] = "yfdrVZ31",
+    ["speaker_receiver.lua"] = "YHHmQcrZ",
+    ["decodificadores.lua"] = "bNEPLHA6",
+    ["atualizador_master.lua"] = "uZ6QV67i",
+    ["server.lua"] = "YfGzJSxG",
+    ["musica.lua"] = "hmG6Ek2R",
+    ["anotacoes.lua"] = "wnu1q4dU",
+    ["planilha.lua"] = "D24zK5zj",
+    ["pocket_client.lua"] = "EAuKVMKB",
+    ["email_server.lua"] = "BqdgQ1ym",
+    ["email_client.lua"] = "Gb7i51mq"
+}
+ 
