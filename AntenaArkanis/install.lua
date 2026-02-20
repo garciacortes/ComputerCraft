eccc1111144ac81fa072cbcfb13ba6c4d0844e65
Lucas Garcia Côrtes <55157556+garciacortes@users.noreply.github.com>
2026-02-20 01:58:37 -0300

    Add install script to download Lua files
    This script downloads specified Lua files from GitHub and saves them locally.

---
+++ AntenaArkanis/install.lua
@@ -0,0 +1,27 @@
+local files = {
+  d20 = "https://raw.githubusercontent.com/garciacortes/ComputerCraft/refs/heads/main/d20Nono/d20.lua",
+  pixelui = "https://raw.githubusercontent.com/garciacortes/ComputerCraft/refs/heads/main/d20Nono/pixelui.lua",
+  shrekbox = "https://raw.githubusercontent.com/garciacortes/ComputerCraft/refs/heads/main/d20Nono/shrekbox.lua"
+}
+
+for name, url in pairs(files) do
+  print("Baixando:", name)
+
+  local res = http.get(url)
+  if res then
+    local file = fs.open(name .. ".lua", "w")
+
+    while true do
+      local chunk = res.read(512)
+      if not chunk then break end
+      file.write(chunk)
+    end
+
+    file.close()
+    res.close()
+
+    print("Ok: ", name)
+  else
+    print("Erro ao baixar:", name)
+  end
+end
